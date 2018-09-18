5f360a966a12a0cad0802a7b8fb6e77951383dd6
Dinesh-Kumaar <dineshkamara@gmail.com>
2018-09-18 20:57:26 +0530

    Finished Almost

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ document.querySelector('form').addEventListener('submit', getBMI);
 // Getting BMI 
 function display(bmi){
     if(bmi < 18.5){
-        document.body.style = "background: green";
+        document.querySelector('p').innerText = ` Your BMI is ${bmi} , your Lean ,Put some More Weight`;
+    } else if(bmi > 18.5 && bmi < 23){
+        document.querySelector('p').innerText = ` Your BMI is ${bmi} , your Normal ,Just  Maintain Your Fitness`;
     }
 }
-function getBMI(){
-    // e.preventDefault();
+function getBMI(e){
+    e.preventDefault();
     // Getting Inputs
     let weight = document.querySelector('#calc-weight').value;
     let height = document.querySelector('#calc-height').value;
     let heightinM = height/100;
     let bmi = (weight/heightinM**2).toFixed(2);
-    document.querySelector('p').innerText = `${bmi}`;
     display(bmi);
 }
