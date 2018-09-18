94d9f7484bdf357fedf7a9f99998f7f97699f7d3
Dinesh-Kumaar <dineshkamara@gmail.com>
2018-09-18 21:07:49 +0530

    Almost Finished

--- app.js
+++ app.js
@@ -2,11 +2,18 @@
 document.querySelector('form').addEventListener('submit', getBMI);
 // Getting BMI 
 function display(bmi){
+    let bmiColor = document.querySelector('.bmi-output');
     if(bmi < 18.5){
         document.querySelector('p').innerText = ` Your BMI is ${bmi} , your Lean ,Put some More Weight`;
+        bmiColor.style.background = " rgb(224, 77, 24)";
     } else if(bmi > 18.5 && bmi < 23){
-        document.querySelector('p').innerText = ` Your BMI is ${bmi} , your Normal ,Just  Maintain Your Fitness`;
+        document.querySelector('p').innerText = ` Your BMI is ${bmi} , your Normal , Just  Maintain Your Fitness`;
+        bmiColor.style.background = " rgb(20, 173, 20)";
+    }  else if(bmi > 23){
+        document.querySelector('p').innerText = ` Your BMI is ${bmi} , your Obese , Go To Gym and Start Training`;
+        bmiColor.style.background = " red";
     }
+
 }
 function getBMI(e){
     e.preventDefault();
